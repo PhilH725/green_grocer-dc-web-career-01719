f3b932357055c73f45a71e763b1c436ff9c9809d
Phil Hartley <philh725@gmail.com>
2019-01-01 19:04:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,6 +53,8 @@ def checkout(cart, coupons)
   new_cart = apply_coupons(new_cart, coupons)
   new_cart = apply_clearance(new_cart)
   
+  puts new_cart
+  puts coupons
   new_cart.each do |food, data|
     if data[:count] > 1
       total += (data[:price] * data[:count])
